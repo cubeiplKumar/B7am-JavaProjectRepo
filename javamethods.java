d09c066f7ae0c3c1b903c66172ea6f7774740eee
cubeiplkumar <trainings.anil@gmail.com>
2021-08-17 03:21:18 +0530

    new changes

---
+++ javamethods.java
@@ -0,0 +1,11 @@
+package com.softwaretestinghelp;
+ 
+public class DemoClass{
+ 
+ public static void main (String[] args){
+    System.out.println("Hello , this is sample program");   
+    double d = Math.random();
+    System.out.println("Random Number:"+d);
+ }
+ 
+}
